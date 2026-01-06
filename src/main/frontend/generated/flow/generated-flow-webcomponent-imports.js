3ad2992524153f5e0c1fb203ba62a83e4553c0d8
dawidczakanski <dawidczakanski@gmail.com>
2026-01-06 13:24:46 +0100

    Zaktualizuj importy i konfigurację logowania: zmiana ścieżek importów w plikach generated-flow-imports.js i generated-flow-webcomponent-imports.js; uproszczenie wzorców logowania w logback.xml; dodanie nowego pliku css.generated.js do zarządzania globalnym CSS

--- src/main/frontend/generated/flow/generated-flow-webcomponent-imports.js
+++ src/main/frontend/generated/flow/generated-flow-webcomponent-imports.js
@@ -9,22 +9,20 @@ import '@vaadin/icons/vaadin-iconset.js';
 import '@vaadin/icon/src/vaadin-icon.js';
 import '@vaadin/common-frontend/ConnectionIndicator.js';
 import 'Frontend/generated/jar-resources/ReactRouterOutletElement.tsx';
-import 'react-router';
-import 'react';
 
 const loadOnDemand = (key) => {
   const pending = [];
   if (key === '5af77f1a06365f154b6a8021652b32a1cd170da07b0ee0e346ae427444938dc5') {
-    pending.push(import('./chunks/chunk-d03fc36af15c22640716f21d248ae8940c74d2aa9e5406f8bd9c6a4ea75701a0.js'));
+    pending.push(import('./chunks/chunk-3ce744bc01937b08dc24601b19e69d2c70a262096a3f8d30a1d90895557d9c10.js'));
   }
   if (key === 'df2b38cb32f6d7ae7d692abf95c719880290adcbf6d769384613c822338ff1b3') {
-    pending.push(import('./chunks/chunk-47cf30d21020d335c4015acb6ba0d09242e8c05158d66026bfd272af5ab0b861.js'));
+    pending.push(import('./chunks/chunk-8335e6935a1aba185f6b2b8e0c6d15e08fa3fe0ea509fcc5c75d1a7dcce8284e.js'));
   }
   if (key === 'bec2593ce6ff00e139f3785d07f5b4041a383a1176c0d48a0001065d6a525f94') {
-    pending.push(import('./chunks/chunk-570b096e7df4e4741625e4c88153b217a14e437ceb14562ef68510cadea343f1.js'));
+    pending.push(import('./chunks/chunk-5854f9a82aa6fe1e23f2287853beac6ea30feea3d049b47e421f59aeeb163c1a.js'));
   }
   if (key === 'f75551a24cad8f070f0f8f53e268edb8dc473c6df602139d27201677e986bf13') {
-    pending.push(import('./chunks/chunk-d3974a2be8ad2242fc93a894265ddced17215061ed742c53885f19faee1dcb2a.js'));
+    pending.push(import('./chunks/chunk-6e4a8597b1ba52a1a130e7098755809fb81d91971fcce28f7519ac0cbb310279.js'));
   }
   return Promise.all(pending);
 }
